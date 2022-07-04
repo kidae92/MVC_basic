3c2a7d0b90decb7e67725511ee683832788e9094
kidae92 <kidae92@hanmail.net>
2022-07-04 16:53:51 +0900

    MemberRepository

---
+++ src/main/java/com/mvc/basic/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package com.mvc.basic.repository;
+
+import com.mvc.basic.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+
+}

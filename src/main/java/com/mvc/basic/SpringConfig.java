f7600a68bde38ec20ec616961e4ce4952da5cd83
kidae92 <kidae92@hanmail.net>
2022-07-06 12:26:18 +0900

    DataSource

--- src/main/java/com/mvc/basic/SpringConfig.java
+++ src/main/java/com/mvc/basic/SpringConfig.java
@@ -1,14 +1,24 @@
 package com.mvc.basic;
 
-import com.mvc.basic.domain.Member;
+
+import com.mvc.basic.repository.JdbcMemberRepository;
+
 import com.mvc.basic.repository.MemberRepository;
-import com.mvc.basic.repository.MemoryMemberRepository;
 import com.mvc.basic.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -17,6 +27,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }

cdf9fa4a9e9489a16cc9297a65e8a9d1e8a01da2
kidae92 <kidae92@hanmail.net>
2022-07-06 12:35:15 +0900

    Transactional 적용

---
+++ src/test/java/com/mvc/basic/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,57 @@
+package com.mvc.basic.service;
+
+import com.mvc.basic.domain.Member;
+import com.mvc.basic.repository.MemberRepository;
+import com.mvc.basic.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional  // Transactional을 선언해주면 테스트 코드가 DB에 반영되지 않는다. 테스트 후 롤백이 된다.
+class MemberServiceIntegrationTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("hello");
+        //when
+        Long saveId = memberService.join(member);
+
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복회원예외() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+        //when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다");
+
+
+    }
+}
